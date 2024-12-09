cdff624714ac6c6bc1ee76eab3c9cd3be4558bc5
Suman Kumar S <suman968.sk@gmail.com>
2024-12-09 13:49:53 -0800

    nm

--- MyFirstTerraformProject/main.tf
+++ MyFirstTerraformProject/main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region="eu-west-1"
 }
 
-resource "aws_s3_bucket" "first_bucket" {
+resource "aws_s3_bucket" "first8217526_bucket" {
   bucket = "suman82175269-first-bucket-newbarun"
 }
